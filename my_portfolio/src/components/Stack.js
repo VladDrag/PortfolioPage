0c700dcda7a2248fa9ac09b6c66529a949afe8b3
VladDrag <vdrag@DESKTOP-49V6C5O.localdomain>
2022-10-11 11:54:42 +0200

    Add full known tech stack

--- my_portfolio/src/components/Stack.js
+++ my_portfolio/src/components/Stack.js
@@ -8,9 +8,12 @@ export default function Stack() {
 				<ul>
 					<li className="stack-element">ASP.NET</li>
 					<li className="stack-element">.NET Core</li>
-					<li className="stack-element">.NET Core</li>
-					<li className="stack-element">.NET Core</li>
-					<li className="stack-element">.NET Core</li>
+					<li className="stack-element">Node.js</li>
+					<li className="stack-element">Rust / Rocket framework</li>
+					<li className="stack-element">Microsoft Azure</li>
+					<li className="stack-element">Razor Pages</li>
+					<li className="stack-element">MongoDB</li>
+					<li className="stack-element">SQL / Azure SQL</li>
 				</ul>
 			</div>
 		</div>
