aeaff38141f2f2ea0efd8c55c7346efb34d1f032
Vlad Dragomir <vdragomir@protonmail.com>
2022-10-12 14:30:36 +0200

    Add one project

--- my_portfolio/src/components/Projects.js
+++ my_portfolio/src/components/Projects.js
@@ -1,8 +1,19 @@
+import push_swap from '../images/push_swap.gif';
 
 export default function Projects() {
 	return (
-		<div className="about">
-			<p>"Hello world proj!"</p>
+		<div className="projects">
+			<>
+				<h3>Push_Swap</h3>
+				<p>
+					The goal of this project is to sort a list of numbers using two stacks and a limited set of instructions. 
+					The program must display the instructions used to sort the list. 
+					The program must also display the number of instructions used to sort the list.
+				</p>
+				<button className="source-code-btn"/>
+				<img src={push_swap} alt="push_swap" className="push_swap"/>
+				
+			</>
 		</div>
 	);
 }
