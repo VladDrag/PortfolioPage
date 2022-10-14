c98c6d4e0b542dc1059eebeab3078c3408003ac8
Vlad Dragomir <vdragomir@protonmail.com>
2022-10-14 10:13:47 +0200

    Add first portfolio

--- my_portfolio/src/components/Projects.js
+++ my_portfolio/src/components/Projects.js
@@ -1,19 +1,22 @@
 import push_swap from '../images/push_swap.gif';
+import './Projects.css';
 
 export default function Projects() {
+	// try to programatically create an array with the details each project and create a Project component in order to store the array data in HTML blabla
 	return (
 		<div className="projects">
-			<>
-				<h3>Push_Swap</h3>
-				<p>
-					The goal of this project is to sort a list of numbers using two stacks and a limited set of instructions. 
-					The program must display the instructions used to sort the list. 
-					The program must also display the number of instructions used to sort the list.
-				</p>
-				<button className="source-code-btn"/>
-				<img src={push_swap} alt="push_swap" className="push_swap"/>
-				
-			</>
+			<div className="project">
+				<h3 className="project__title">Push_Swap</h3>
+				<div className="project__card">
+					<p className="project__description">
+						The goal of this project is to sort a list of numbers using two stacks and a limited set of instructions. 
+						The program must display the instructions used to sort the list. 
+						The program must also display the number of instructions used to sort the list.
+					</p>
+					<img className="project__img" src={push_swap} alt="push_swap"/>
+				</div>
+				<a href="" className="project__link"/>
+			</div>
 		</div>
 	);
 }
